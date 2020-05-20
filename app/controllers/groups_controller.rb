e9a9bed380c63f5646e7aa0562af922fdbd2dcce
ian293382 <moshou5571228@gmail.com>
2020-05-20 19:20:14 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :join, :quit]
 
 
   def index
@@ -35,6 +35,34 @@ class GroupsController < ApplicationController
       @group.destroy
       redirect_to groups_path
     end
+
+    def join
+      @group = Group.find(params[:id])
+
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "加入本討論版成功"
+      else
+        flash[:warning] = "你已經是本討論版成員"
+      end
+
+      redirect_to group_path(@group)
+    end
+
+
+    def quit
+      @group = Group.find(params[:id])
+
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert] = "已經退出本討論版"
+      else
+        flash[:warning] = "你不是是本討論版成員,無法退出"
+      end
+
+      redirect_to group_path(@group)
+    end
+
 
     private
 
